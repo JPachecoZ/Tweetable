cecd88fb490a293ffa2df3e0f6788973e03327c1
Javier <jepach@gmail.com>
2022-02-02 17:51:29 -0500

    Added Stylesheets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,13 +2,14 @@ class Tweet < ApplicationRecord
   # Validations
 
   validates :body, null: false, presence: true, maxlength: 140
-  
+
   # Associations
 
   belongs_to :user
 
   has_many :replies, class_name: "Tweet",
                       foreign_key: "replied_to_id"
+  has_many :likes, dependent: :destroy
 
   belongs_to :replied_to, class_name: "Tweet", optional: true
 end
