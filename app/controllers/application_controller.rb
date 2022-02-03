bd5cadd3eba355988d17471f5b1b198fe4aafa5f
Javier <jepach@gmail.com>
2022-02-03 09:45:51 -0500

    Fixed issue where Create account didn't redirect to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :avatar])
   end
 end
