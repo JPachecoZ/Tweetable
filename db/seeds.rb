00b4e8abbea2d9099fe371f8cb0d29a699efb2fa
Tommy Vasquez <tommyvasquezr@gmail.com>
2022-02-03 12:52:37 -0500

    Add show route

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,10 @@ User.destroy_all
 Tweet.destroy_all
 
 puts "Seeding companies"
-admin = User.create(email: "admin@mail.com", password: "admin123", username: "admin", name: "admin", role: 1)
-tweet1 = admin.tweets.create(body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita modi numquam tempore autem! Ea consequatur.")
-tweet2 = admin.tweets.create(body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita modi numquam tempore autem! Ea consequatur.")
-tweet3 = admin.tweets.create(body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita modi numquam tempore autem! Ea consequatur.")
+admin = User.create!(email: "admin@mail.com", password: "admin123", username: "admin", name: "admin", role: 1)
+tweet1 = admin.tweets.create!(body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita modi numquam tempore autem! Ea consequatur.")
+tweet2 = admin.tweets.create!(body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita modi numquam tempore autem! Ea consequatur.")
+tweet3 = admin.tweets.create!(body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita modi numquam tempore autem! Ea consequatur.")
 
 admin.avatar.attach(io: File.open("db/images/avatar.jpg"), filename: "avatar")
 
