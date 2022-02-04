7e5b822ce323e155e9dd359d76ae9acfed19f740
Javier <jepach@gmail.com>
2022-02-03 19:42:05 -0500

    Added omniauth features

--- Gemfile
+++ Gemfile
@@ -52,6 +52,10 @@ gem "devise"
 
 gem "omniauth-github", github: "omniauth/omniauth-github", branch: "master"
 
+gem "omniauth-rails_csrf_protection"
+
+gem "net-http"
+
 # Use Sass to process CSS
 # gem "sassc-rails"
 
@@ -61,6 +65,7 @@ gem "omniauth-github", github: "omniauth/omniauth-github", branch: "master"
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri mingw x64_mingw]
+  gem "dotenv-rails"
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_205228) do
+ActiveRecord::Schema.define(version: 2022_02_03_232749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2022_02_02_205228) do
     t.integer "role", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
