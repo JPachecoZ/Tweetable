f6fce6d2af960cb161b1376b0bbf6f0fa42c57c8
eduardo4394 <eduardo_sh@outlook.com>
2022-02-05 10:46:08 -0500

    fix reply bug order index and show tweet

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_02_03_141036) do
-
+ActiveRecord::Schema.define(version: 2022_02_03_232749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,7 +79,6 @@ ActiveRecord::Schema.define(version: 2022_02_03_141036) do
     t.integer "likes_count", default: 0
     t.string "provider"
     t.string "uid"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
